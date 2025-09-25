1039ffec18af47e8dc5c5e73e4d37ce3d82a810e
Madsbert <mberthelsen@live.dk>
2025-09-25 10:21:40 +0200

    Add project files.

---
+++ ImageClassificationConsole/2_Application/application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageClassificationConsole._2_Application
+{
+    public class application
+    {
+
+        public double CalculateAccuracy()
+        { return 0; }
+
+        public double CalculatePrecision()
+        { return 0; }
+        public double CalculateF1()
+        { return 0; }
+        public double CalculateRecall()
+        { return 0; }
+        public double SelectPicture()
+        { return 0; }
+
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using ImageClassificationConsole._2_Application;
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+
+        //Arrenge 
+        application _application= new application();
+        [Fact]
+        public void TestAccuracyOfAI()
+        {
+            //Arrenge
+            var expectedResult = 85;
+            //Act
+            var actualResult = _application.CalculateAccuracy();
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+
+        [Fact]
+        public void TestPrecisionOfAI()
+        {
+            //Arrenge
+            var expectedResult = 85;
+            //Act
+            var actualResult = _application.CalculatePrecision();
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Fact]
+        public void TestRecallOfAI()
+        {
+            //Arrenge
+            var expectedResult = 85;
+            //Act
+            var actualResult = _application.CalculateRecall();
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Fact]
+        public void TestF1OfAI()
+        {
+            //Arrenge
+            var expectedResult = 85;
+            //Act
+            var actualResult = _application.CalculateF1();
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Fact]
+        public void TestCorrectPicture()
+        {
+            //arrange
+            bool HasBeenOpened = false;
+
+            //Act
+            var selectedPNG = _application.SelectPicture();
+
+            if (selectedPNG != null)
+            {
+                HasBeenOpened = true;
+            }
+
+            //Assert
+            Assert.True(HasBeenOpened);
+        }
+    }
+}
